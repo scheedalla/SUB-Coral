15ef1f1afbbd2e6d001707ab6bdc0c3064f12ab4
Cheedalla, Sruti <Sruti.Cheedalla@washpost.com>
2016-04-15 12:34:32 -0400

    added db_local.js to config

---
+++ config/db_local.js
@@ -0,0 +1,9 @@
+mongodb://localhost:27017/
+
+// config/db.js  This is where you would put in your mongodb connection information
+module.exports = {
+    url : 'mongodb://localhost:27017/',    
+    assetsBase:'/',
+    applicationBase:'//localhost:3000/'
+
+}
