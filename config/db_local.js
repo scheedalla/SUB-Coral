ed3d23df1352ee711108126357da0f82166d6728
styonsk <styonsk@gmail.com>
2016-04-15 12:27:54 -0400

    Add db_local config to repo

---
+++ config/db_local.js
@@ -0,0 +1,6 @@
+// db_local.js  This is where you would put in your mongodb connection information
+module.exports = {
+    url : 'mongodb://sub_user:test1234@localhost:27017/sub?poolSize=10',
+    assetsBase:'/',
+    applicationBase:'//localhost:3000/'
+}
